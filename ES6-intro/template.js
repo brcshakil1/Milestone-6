765a4baa681f7533dd58845fe213287e1abbb80d
brcshakil1 <brcshakil72@gmail.com>
2023-08-23 12:28:01 +0600

    template string

---
+++ ES6-intro/template.js
@@ -0,0 +1,23 @@
+const first = 'Bayazid';
+const second = 'second';
+const greet = 'Good Morning';
+
+const fullName = first + ' ' + second + ' ' + greet;
+
+const a = 85;
+const b = 75;
+const result = 'The sum of ' + a + ' and ' + b + ' is ' + (a+b);
+
+const numbers = [45, 78, 89, 185];
+const person = {name: 'John', age: numbers[2]}
+
+// const math = `The sum of ${a} and ${b} is ${a+b}`;
+const math = `The sum of ${numbers[3]} and ${person.age} is ${a+b}`;
+
+
+const email = 'Hi Roman \n' + 
+'tumi mara mari korbe naki';
+
+const email2 = `Hi Roman
+Asho mara mari kori`
+console.log(math);
