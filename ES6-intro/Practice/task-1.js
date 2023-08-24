1ad5e1fd0ffc56c7696e67fc3f442df43d8e8284
brcshakil1 <brcshakil72@gmail.com>
2023-08-24 08:19:14 +0600

    practice task

---
+++ ES6-intro/Practice/task-1.js
@@ -0,0 +1,18 @@
+// Task 1
+const multiply = (num1, num2, num3) => num1 * num2 * num3;
+// console.log(multiply(5, 2, 7));
+
+// Task 2
+const myself = `I am a developer.
+I love to code.
+I love to eat biryani.`;
+// console.log(myself);
+
+// Task 3 
+const getFullName = (firstName, lastName = 'Chowdhury') => `${firstName} ${lastName}`;
+// console.log(getFullName('Bayazidur Rahman'));
+
+
+
+
+
