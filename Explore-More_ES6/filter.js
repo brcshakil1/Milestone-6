e2449d4feb04c70939b62764546ce754d8ffbeff
brcshakil1 <brcshakil72@gmail.com>
2023-08-24 13:03:02 +0600

    map, forEach and filter

---
+++ Explore-More_ES6/filter.js
@@ -0,0 +1,6 @@
+// filter selects elements based on a condition and return an array with the elements that fulfilled the condition
+const numbers = [12, 23, 34, 45, 56, 67];
+const players = [72,65,77,58,98]
+const selected =  players.filter(p => p >70);
+
+console.log(selected)
