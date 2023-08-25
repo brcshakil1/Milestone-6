8a1b82d1c26fe5369fece1b2d3d58c9b11fa5f5e
brcshakil1 <brcshakil72@gmail.com>
2023-08-25 15:48:03 +0600

    added

---
+++ common-js-concepts/true-false.js
@@ -0,0 +1,29 @@
+/**
+ * Truthy ---->
+ * 1. true value is true
+ * 2. any number is true
+ * 3. any string is true
+ * 4. all object is true even empty object
+ * 5. all array is true even array object
+ * 
+ * Falsy ---->
+ * 1. false value is false
+ * 2. 0 is false
+ * 3. '' (empty string) is false
+ * 4. undefined is false 
+ * 5. null is false
+ * 
+ */
+const x = undefined;
+
+if(x) {
+    console.log('Value of x is truthy');
+} else {
+    console.log('value of x is falsy');
+}
+
+if(!x) {
+    console.log('false')
+} else {
+    console.log('true')
+}
